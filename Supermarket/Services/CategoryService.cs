548f15b5682fd4d57b4e1cefba434849f2d94325
Akwasi Appiah <Akwasi.Appiah@capitalontap.com>
2021-10-12 15:58:21 +0100

    Add Category service (to add repo communication later)

---
+++ Supermarket/Services/CategoryService.cs
@@ -0,0 +1,16 @@
+﻿using Supermarket.Domain.Models;
+using Supermarket.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Supermarket.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        public async Task<IEnumerable<Category>> ListAsync()
+        {
+        }
+    }
+}
