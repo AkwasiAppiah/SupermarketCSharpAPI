dadb53083d5a8c4956fad288b326eeb411626ce2
Akwasi Appiah <Akwasi.Appiah@capitalontap.com>
2021-10-12 16:16:57 +0100

    Created Dependency "Injection" and altered Program to permit this change

--- Supermarket/Startup.cs
+++ Supermarket/Startup.cs
@@ -2,10 +2,16 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Supermarket.Domain.Repositories;
+using Supermarket.Domain.Services;
+using Supermarket.Persistence.Contexts;
+using Supermarket.Persistence.Repositories;
+using Supermarket.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,20 +21,25 @@ namespace Supermarket
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<AppDbContext>(options => {
+                options.UseInMemoryDatabase("supermarket-api-in-memory");
+            });
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<ICategoryService, CategoryService>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
